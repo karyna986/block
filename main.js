4ab247ca3e77b1b1fba0cd1483d5c00cee270873
Shavyrina Karyna Aleksandrovna <87013231+karyna986@users.noreply.github.com>
2022-11-14 21:11:45 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let box = document.querySelector('.box') 
+let newBlock = document.createElement('div')
+newBlock.classList = ('mini')
+
+box.append(newBlock)
+
+
+onclick="creatBlock()" - запускає функцію creatBlock() при натисканні (додавати тільки в HTML)
